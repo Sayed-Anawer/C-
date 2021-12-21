5834bbf7c5f7155611d3d17df0d72a6836197cbe
Sayed-Anawer <sayedanawer99@gmail.com>
2021-12-21 17:39:17 +0600

    Grading System with nested loop

---
+++ testGrade.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int number,sum=0;
+
+    for(int i = 0; i<3;i++){
+        do{
+            cout << "Grade " << i+1 << " : ";
+            cin >> number;
+        }while(number<1 || number>5);
+
+        sum+= number;
+    }
+    cout << "Sum is: " << sum << endl;
+    cout << "Average is: "<< (float)sum / 3.0;
+}
